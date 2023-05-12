17bafe67ead2262048eb5178643fecbde16faffd
Kotte Mohankrishna <kottemohankrishna19@saveetha.com>
2023-05-12 20:03:46 +0530

    Initial commit

--- scripts/deploy_and_create.py
+++ scripts/deploy_and_create.py
@@ -1,6 +1,5 @@
 from brownie import SimpleCollectible, accounts, network, config
 from scripts.helpfulscripts import get_account
-from web3 import Web3
 
 sample_token_uri = "ipfs://Qmd9MCGtdVz2miNumBHDbvj8bigSgTwnr4SbyH6DNnpWdt?filename=0-PUG.json"
 OPENSEA_URL = "https://testnets.opensea.io//assets/{}/{}"
@@ -9,12 +8,9 @@ def deploy_create():
     simple_collectible = SimpleCollectible.deploy({"from":account}) 
     tx = simple_collectible.createCollectible(sample_token_uri, {"from":account})
     tx.wait(1)
-    print(f"Awesome you can see your NFT at {OPENSEA_URL.format(simple_collectible.address,simple_collectible.tokenCounter() - 1)}")  
     print(account)
-    web3 = Web3(Web3.HTTPProvider("https://rinkeby.infura.io/v3/754f2d41981e41e99ea338b6ea063bc7"))
-    print(web3.isConnected())
-    return simple_collectible
-
+    print(f"Awesome you can see your NFT at {OPENSEA_URL.format(simple_collectible.address,simple_collectible.tokenCounter() - 1)}")  
+   
 def main():
     deploy_create() 
      
